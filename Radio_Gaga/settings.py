a3ed045ce9c3d63c2d28df2f072347c8e8837603
Diego Correa <diegocorrea.cc@gmail.com>
2018-06-30 16:15:36 -0300

    add settings

--- Radio_Gaga/settings.py
+++ Radio_Gaga/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bahia'
 
 USE_I18N = True
 
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Web settings
+ALLOWED_HOSTS = ['127.0.0.1', '*']
